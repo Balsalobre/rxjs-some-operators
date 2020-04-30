f3bc7259bf021c3eb72e12988ff01e825bc627e5
balsalobre <carlosocb@gmail.com>
2020-04-30 18:41:14 +0200

    fromevent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, timer } from 'rxjs';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+
   title = 'angular-rxjs';
+
+  @ViewChild('myElement') myElement: ElementRef;
+
+  constructor(private renderer: Renderer2) {
+  }
 
   ngOnInit(): void {
 
-    const contador = interval(1000);
+    // la potencia que tiene esto es que nos podemos "desuscribir" del observable cuando queramos
+    const el = document.getElementById('element');
+    const mouseMove = fromEvent(el, 'mousemove');
+    mouseMove.subscribe((e: MouseEvent) => console.log(`Coordenadas: x: ${e.clientX}, y: ${e.clientY}`));
+  }
 
-    contador.subscribe((n) => console.log(`Llevamos ${n} segundos...`));
-
-    const trigger = timer(2000);
-
-    trigger.subscribe(() => console.log('Triggered 2 seg...'));
+  ngAfterViewInit(): void {
+    console.log('>>>>', this.myElement);
+    this.renderer.listen(this.myElement.nativeElement, 'click', (event: MouseEvent) => {
+      console.log('CLICK', event);
+    });
   }
 }
