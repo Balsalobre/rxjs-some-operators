c84c3488b28661fa2f75d83c3e83851ff20f08e7
balsalobre <carlosocb@gmail.com>
2020-04-30 17:13:39 +0200

    recordando observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-rxjs';
+
+  obs: Observable<any>;
+
+  ngOnInit(): void {
+    this.obs = new Observable((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+
+      setTimeout(() => {
+        observer.next(4);
+        // throw new Error('This is not an error');
+        observer.complete();
+      }, 1000);
+    });
+
+    // this.obs.subscribe(console.log);
+
+    this.obs.subscribe({
+      next: x => console.log('Obtenemos el valor: ' + x),
+      error: err => console.error('Error:' + err),
+      complete: () => console.log('COMPLETADO'),
+    });
+  }
 }
