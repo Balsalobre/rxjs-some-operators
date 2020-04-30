4fb677eae545c8ce0be0f2ebf8e0f270d8f113fb
balsalobre <carlosocb@gmail.com>
2020-04-30 17:24:27 +0200

    interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-rxjs';
+
+  ngOnInit(): void {
+
+    const contador = interval(1000);
+
+    contador.subscribe({
+      next: (n) => console.log(`Llevamos ${n} segundos...`),
+      error: e => console.error(e)
+    });
+  }
 }
